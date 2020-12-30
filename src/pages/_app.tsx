d51469d39f5b97f17742ed7929e925fd76dafb1c
WEB DEVELOPER <buka4rill@gmail.com>
2020-12-30 14:21:34 +0100

    Added register page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,30 @@
 import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react'
+import theme from '../theme';
+import { Provider, createClient } from 'urql';
 
-import theme from '../theme'
+const client = createClient({
+  // Our GraphQl server
+  url: 'http://localhost:4000/graphql',
+  fetchOptions: {
+    credentials: "include",
+  }
+});
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider resetCSS theme={theme}>
-      <ColorModeProvider
-        options={{
-          useSystemColorMode: true,
-        }}
-      >
-        <Component {...pageProps} />
-      </ColorModeProvider>
-    </ChakraProvider>
+    // URQL Provider
+    <Provider value={client}> 
+      <ChakraProvider resetCSS theme={theme}>
+        <ColorModeProvider
+          options={{
+            useSystemColorMode: true,
+          }}
+        >
+          <Component {...pageProps} />
+        </ColorModeProvider>
+      </ChakraProvider>
+    </Provider>
+    
   )
 }
 
